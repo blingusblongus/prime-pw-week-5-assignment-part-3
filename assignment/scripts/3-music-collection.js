23f587972b3158dbf43202de3c6ae62f6a8cf4ef
Nick Allen <rutherford.t.b@gmail.com>
2021-09-13 16:58:45 -0500

    created collection array and addToCollection()

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  const obj = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+
+  collection.push(obj);
+  return obj;
+}
