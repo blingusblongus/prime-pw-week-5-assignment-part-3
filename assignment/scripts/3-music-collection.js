5c85b8741f15ea87a2c5cb421da1f0f265fa8c83
Nick Allen <rutherford.t.b@gmail.com>
2021-09-14 15:37:51 -0500

    added test tracklist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,7 +2,7 @@ console.log('***** Music Collection *****')
 
 const collection = [];
 
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, tracks) {
   const obj = {
     title: title,
     artist: artist,
@@ -103,7 +103,49 @@ console.log('\nTest multi-prop search==========');
 console.log(search({artist: 'Thundercat', yearPublished: 2009})); //should return 1
 console.log('\nTest no-prop search=============');
 console.log(search({}));
-//test
 
 
-//start addTracks() ========================
+const apocalypseTracks = [{
+  trackName: 'Tenfold',
+  duration: '3:04'
+},
+{
+  trackName: 'Heartbreaks + Setbacks',
+  duration: '3:23'
+},
+{
+  trackName: 'The Life Aquatic',
+  duration: '2:36'
+},
+{
+  trackName: 'Special Stage',
+  duration: '2:56'
+},
+{
+  trackName: 'Tron Song',
+  duration: '2:34'
+},
+{
+  trackName: 'Seven',
+  duration: '2:16'
+},
+{
+  trackName: 'Oh Sheit It\'s X',
+  duration: '3:47'
+},
+{
+  trackName: 'Without You',
+  duration: '4:41'
+},{
+  trackName: 'Lotus and the Jondy',
+  duration: '4:52'
+},{
+  trackName: 'Evangelion',
+  duration: '2:20'
+},{
+  trackName: 'We\'ll Die',
+  duration: '0:55'
+},{
+  trackName: 'A Message for Austin/Praise the Lord/Enter the Void',
+  duration: '6:35'
+}];
