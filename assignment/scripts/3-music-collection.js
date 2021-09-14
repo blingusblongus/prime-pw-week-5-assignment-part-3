b16cca2357798764a7982b12ca54f797f53b5e70
Nick Allen <rutherford.t.b@gmail.com>
2021-09-14 13:32:08 -0500

    tested with all 6 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,3 +12,13 @@ function addToCollection(title, artist, yearPublished) {
   collection.push(obj);
   return obj;
 }
+
+addToCollection('Apocalypse', 'Thundercat', 2009);
+addToCollection('7Summers', 'Shaun Martin', 2015);
+addToCollection('Motivational Music for the Syncopated Soul', 'Cory Wong', 2019);
+addToCollection('The Beyond / Where the Giants Roam', 'Thundercat', 2015);
+addToCollection('The Optimist', 'Cory Wong', 2018);
+addToCollection('Covers', 'Dirty Loops', 2014);
+
+
+console.log(collection);
