0ac497baee671c29f1965856d73df7457dcc0120
Nick Allen <rutherford.t.b@gmail.com>
2021-09-14 14:01:12 -0500

    created and tested showCollection(

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,6 +13,7 @@ function addToCollection(title, artist, yearPublished) {
   return obj;
 }
 
+//add the albums to the collection
 addToCollection('Apocalypse', 'Thundercat', 2009);
 addToCollection('7Summers', 'Shaun Martin', 2015);
 addToCollection('Motivational Music for the Syncopated Soul', 'Cory Wong', 2019);
@@ -20,5 +21,24 @@ addToCollection('The Beyond / Where the Giants Roam', 'Thundercat', 2015);
 addToCollection('The Optimist', 'Cory Wong', 2018);
 addToCollection('Covers', 'Dirty Loops', 2014);
 
+//testing addToCollection
+//console.log(collection);
+collection.forEach((obj) => console.log(obj));
 
-console.log(collection);
+
+function showCollection(array){
+  //return number of items in array
+  console.log('Number of items:', array.length);
+
+  //display info:
+  for(let obj of array){
+    const title = obj.title;
+    const artist = obj.artist;
+    const year = obj.yearPublished;
+
+    console.log(`${title} by ${artist}, published in ${year}`)
+  }
+}
+
+//testing showCollection
+showCollection(collection);
