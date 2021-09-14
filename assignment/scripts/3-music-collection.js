fd0e69c7a6406e01f3757b864ab3312baf494636
Nick Allen <rutherford.t.b@gmail.com>
2021-09-14 15:41:19 -0500

    implemented/tested addToCollection with tracks

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -6,7 +6,8 @@ function addToCollection(title, artist, yearPublished, tracks) {
   const obj = {
     title: title,
     artist: artist,
-    yearPublished: yearPublished
+    yearPublished: yearPublished,
+    tracks: tracks
   };
 
   collection.push(obj);
@@ -14,7 +15,50 @@ function addToCollection(title, artist, yearPublished, tracks) {
 }
 
 //add the albums to the collection
-addToCollection('Apocalypse', 'Thundercat', 2009);
+addToCollection('Apocalypse', 'Thundercat', 2009, [{
+  trackName: 'Tenfold',
+  duration: '3:04'
+},
+{
+  trackName: 'Heartbreaks + Setbacks',
+  duration: '3:23'
+},
+{
+  trackName: 'The Life Aquatic',
+  duration: '2:36'
+},
+{
+  trackName: 'Special Stage',
+  duration: '2:56'
+},
+{
+  trackName: 'Tron Song',
+  duration: '2:34'
+},
+{
+  trackName: 'Seven',
+  duration: '2:16'
+},
+{
+  trackName: 'Oh Sheit It\'s X',
+  duration: '3:47'
+},
+{
+  trackName: 'Without You',
+  duration: '4:41'
+},{
+  trackName: 'Lotus and the Jondy',
+  duration: '4:52'
+},{
+  trackName: 'Evangelion',
+  duration: '2:20'
+},{
+  trackName: 'We\'ll Die',
+  duration: '0:55'
+},{
+  trackName: 'A Message for Austin/Praise the Lord/Enter the Void',
+  duration: '6:35'
+}]);
 addToCollection('7Summers', 'Shaun Martin', 2015);
 addToCollection('Motivational Music for the Syncopated Soul', 'Cory Wong', 2019);
 addToCollection('The Beyond / Where the Giants Roam', 'Thundercat', 2015);
